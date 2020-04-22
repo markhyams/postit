6eae19dc838c013199d1cb84d634f31ad311b59a
Mark Hyams <markhyams@gmail.com>
2020-04-22 16:09:57 -0600

    users and authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200420192710) do
+ActiveRecord::Schema.define(version: 20200422191925) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,12 +25,6 @@ ActiveRecord::Schema.define(version: 20200420192710) do
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "dogs", force: true do |t|
-    t.string  "name"
-    t.integer "height_in_inches"
-    t.integer "weight_in_lbs"
   end
 
   create_table "post_categories", force: true do |t|
@@ -53,6 +47,7 @@ ActiveRecord::Schema.define(version: 20200420192710) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
